5a4bab28266ce9339f0ca39d714c769265419d38
Victor Manuel <britoanicavictormanuel@gmail.com>
2025-06-08 21:33:22 -0600

    feat: add navigation drawer

---
+++ app/src/main/java/com/sipues/ui/search/SearcScreen.kt
@@ -0,0 +1,12 @@
+package com.sipues.ui.search
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SearchScreen(
+    onBack: () -> Unit,
+) {
+    Text("Pantalla de Búsqueda", modifier = Modifier)
+}
